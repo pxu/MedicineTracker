8a2d2ed4120b968816e9bb59b0c8f701081b7163
Xu <xup@student.douglascollege.ca>
2017-07-15 13:36:26 -0700

    Initial commit

---
+++ app/src/main/java/com/douglascollege/a300216962/medicinetracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.douglascollege.a300216962.medicinetracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
